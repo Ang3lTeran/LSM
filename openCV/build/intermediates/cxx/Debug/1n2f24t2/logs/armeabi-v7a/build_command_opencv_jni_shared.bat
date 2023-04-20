282c24efabf4c3e55d0371b65b0b4506fada7836
Jim Morrison <ang3ltp@gmail.com>
2023-04-20 00:01:56 -0600

    importacion de libreria opencv

---
+++ openCV/build/intermediates/cxx/Debug/1n2f24t2/logs/armeabi-v7a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\angel\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Documents\\openCV\\openCV\\.cxx\\Debug\\1n2f24t2\\armeabi-v7a" ^
+  opencv_jni_shared
---
+++ openCV/build/intermediates/cxx/Debug/1n2f24t2/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\angel\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Documents\\openCV\\openCV\\.cxx\\Debug\\1n2f24t2\\x86_64" ^
+  opencv_jni_shared
